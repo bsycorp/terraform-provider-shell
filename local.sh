98fc23588ac61a8bc17948c73724dea1135cdb0a
nhoughto <nhoughto@gmail.com>
2018-11-01 19:41:54 +1100

    Make provider executable

--- local.sh
+++ local.sh
@@ -2,8 +2,10 @@
 VERSION="0.1.0"
 echo "Downloading plugin to ~/.terraform.d/plugins/.."
 mkdir -p ~/.terraform.d/plugins/darwin_amd64
-(cd ~/.terraform.d/plugins/darwin_amd64; curl -sSL https://github.com/bsycorp/terraform-provider-shell/releases/download/$VERSION/terraform-provider-shell_v$VERSION-darwin-amd64 -o terraform-provider-shell_v$VERSION)
+curl -sSL https://github.com/bsycorp/terraform-provider-shell/releases/download/$VERSION/terraform-provider-shell_v$VERSION-darwin-amd64 -o ~/.terraform.d/plugins/darwin_amd64/terraform-provider-shell_v$VERSION
+chmod +x ~/.terraform.d/plugins/darwin_amd64/terraform-provider-shell_v$VERSION
 
 mkdir -p ~/.terraform.d/plugins/linux_amd64
-(cd ~/.terraform.d/plugins/linux_amd64; curl -sSL https://github.com/bsycorp/terraform-provider-shell/releases/download/$VERSION/terraform-provider-shell_v$VERSION-linux-amd64 -o terraform-provider-shell_v$VERSION)
+curl -sSL https://github.com/bsycorp/terraform-provider-shell/releases/download/$VERSION/terraform-provider-shell_v$VERSION-linux-amd64 -o ~/.terraform.d/plugins/linux_amd64/terraform-provider-shell_v$VERSION
+chmod +x ~/.terraform.d/plugins/linux_amd64/terraform-provider-shell_v$VERSION
 echo "Done"
